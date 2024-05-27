cd5905308c9b860dd7a6a7cf9da897f757842866
jugviana <julianagv.jgv@gmail.com>
2024-05-27 10:03:25 -0300

    add keyboard navigation tests

--- src/tabsComponent.test.tsx
+++ src/tabsComponent.test.tsx
@@ -39,11 +39,16 @@ const RenderTabsComponent = () => {
 };
 
 describe("Tabs Component", () => {
-  test("renders the component with the first tab and panel actived", async () => {
+  let tab0: HTMLElement, tab1: HTMLElement, tab2: HTMLElement;
+
+  beforeEach(() => {
     render(<RenderTabsComponent />);
-    const tab0 = screen.getByRole("tab", { name: "Tab0" });
-    const tab1 = screen.getByRole("tab", { name: "Tab1" });
-    const tab2 = screen.getByRole("tab", { name: "Tab2" });
+    tab0 = screen.getByRole("tab", { name: "Tab0" });
+    tab1 = screen.getByRole("tab", { name: "Tab1" });
+    tab2 = screen.getByRole("tab", { name: "Tab2" });
+  });
+
+  test("renders the component with the first tab and panel actived", async () => {
     expect(tab0).toHaveAttribute("aria-selected", "true");
     expect(tab1).toHaveAttribute("aria-selected", "false");
     expect(tab2).toHaveAttribute("aria-selected", "false");
@@ -54,10 +59,6 @@ describe("Tabs Component", () => {
   });
 
   test("activates the second tab and panel when the second tab button is clicked", async () => {
-    render(<RenderTabsComponent />);
-    const tab0 = screen.getByRole("tab", { name: "Tab0" });
-    const tab1 = screen.getByRole("tab", { name: "Tab1" });
-    const tab2 = screen.getByRole("tab", { name: "Tab2" });
     await userEvent.click(tab1);
     expect(tab1).toHaveAttribute("aria-selected", "true");
     expect(tab0).toHaveAttribute("aria-selected", "false");
@@ -69,10 +70,6 @@ describe("Tabs Component", () => {
   });
 
   test("activates the third tab and panel when the third tab button is clicked", async () => {
-    render(<RenderTabsComponent />);
-    const tab0 = screen.getByRole("tab", { name: "Tab0" });
-    const tab1 = screen.getByRole("tab", { name: "Tab1" });
-    const tab2 = screen.getByRole("tab", { name: "Tab2" });
     await userEvent.click(tab2);
     expect(tab2).toHaveAttribute("aria-selected", "true");
     expect(tab0).toHaveAttribute("aria-selected", "false");
@@ -81,5 +78,79 @@ describe("Tabs Component", () => {
     expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
     expect(screen.queryByText("Panel1")).not.toBeInTheDocument();
     expect(screen.getByText("Panel2")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("activates the second tab when arrow right is pressed", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{ArrowRight}");
+    expect(tab1).toHaveAttribute("aria-selected", "true");
+    expect(tab0).toHaveAttribute("aria-selected", "false");
+    expect(tab2).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByText("Panel1")).toBeVisible();
+    expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
+    expect(screen.queryByText("Panel2")).not.toBeInTheDocument();
+    expect(screen.getByText("Panel1")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("activates the third tab when arrow right is pressed", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{ArrowRight}");
+    await userEvent.keyboard("{ArrowRight}");
+    expect(tab1).toHaveAttribute("aria-selected", "false");
+    expect(tab0).toHaveAttribute("aria-selected", "false");
+    expect(tab2).toHaveAttribute("aria-selected", "true");
+    expect(screen.getByText("Panel2")).toBeVisible();
+    expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
+    expect(screen.queryByText("Panel1")).not.toBeInTheDocument();
+    expect(screen.getByText("Panel2")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("activates the last tab when arrow left is pressed on the first tab", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{ArrowLeft}");
+    expect(tab2).toHaveAttribute("aria-selected", "true");
+    expect(tab0).toHaveAttribute("aria-selected", "false");
+    expect(tab1).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByText("Panel2")).toBeVisible();
+    expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
+    expect(screen.queryByText("Panel1")).not.toBeInTheDocument();
+    expect(screen.getByText("Panel2")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("activates the second tab when arrow down is pressed on the first tab", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{ArrowDown}");
+    expect(tab0).toHaveAttribute("aria-selected", "false");
+    expect(tab1).toHaveAttribute("aria-selected", "true");
+    expect(tab2).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByText("Panel1")).toBeVisible();
+    expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
+    expect(screen.queryByText("Panel2")).not.toBeInTheDocument();
+    expect(screen.getByText("Panel1")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("activates the last tab when arrow up is pressed on the first tab", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{ArrowUp}");
+    expect(tab0).toHaveAttribute("aria-selected", "false");
+    expect(tab1).toHaveAttribute("aria-selected", "false");
+    expect(tab2).toHaveAttribute("aria-selected", "true");
+    expect(screen.getByText("Panel2")).toBeVisible();
+    expect(screen.queryByText("Panel0")).not.toBeInTheDocument();
+    expect(screen.queryByText("Panel1")).not.toBeInTheDocument();
+    expect(screen.getByText("Panel2")).toHaveAttribute("tabindex", "0");
+  });
+
+  test("Focus on the content of the first tab when tab button is pressed", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{Tab}");
+    expect(screen.getByText("Panel0")).toHaveFocus();
+  });
+
+  test("activates the previous focusable element when shift tab is pressed", async () => {
+    tab0.focus();
+    await userEvent.keyboard("{Shift>}{Tab}");
+    expect(tab0).toHaveAttribute("aria-selected", "true");
+    expect(screen.getByText("Panel0")).toHaveAttribute("tabindex", "0");
   });
 });
